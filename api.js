8b0195c2d71277e94162a0fd8baaf5f13c88d27a
maugrim777 <gabriel.vlasceanu@outlook.com>
2020-05-21 09:27:05 +0300

    first commit

---
+++ api.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+const topics = require('./publicForum.json')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/topics', (req,res) => res.json(topics))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+
